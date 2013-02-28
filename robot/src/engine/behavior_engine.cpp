65b797907d8df9827158560ef57ce0fd81c29abf
adrif <anass.drif@gmail.com>
2013-02-28 20:42:22 +0100

    Noise detection callback added cnt.

--- robot/src/engine/behavior_engine.cpp
+++ robot/src/engine/behavior_engine.cpp
@@ -1,3 +1,6 @@
+#include <alvalue/alvalue.h>
+#include <alcommon/alproxy.h>
+#include <alcommon/albroker.h>
 #include "behavior_engine.hpp"
 #include <stdint.h>
 
@@ -12,7 +15,51 @@ namespace robotutor {
 	BehaviorEngine::BehaviorEngine(ScriptEngine & parent, boost::asio::io_service & ios, boost::shared_ptr<AL::ALBroker> broker) :
 		parent_(parent),
 		ios_(ios),
-		bm_(broker) {}
+		bm_(broker)
+		ALSoundExtractor(broker, name) {}
+		
+	void BehaviorEngine::init() {
+	  
+	  audioDevice->callVoid("setClientPreferences",
+	                        getName(),                //Name of this module
+	                        16000,                    //16000 Hz requested
+	                        (int)FRONTCHANNEL,        //Front Channels requested
+	                        0                         //Deinterleaving is not needed here
+	                        );
+	
+	  startDetection();
+	}
+		
+	BehaviorEngine::~BehaviorEngine() {
+		stopDetection();
+	}
+	
+	/// This function will be automatically called by the module ALAudioDevice
+	/// every 170ms with the appropriate audio buffer (front channel at 16000Hz)
+	void BehaviorEngine::noiseCallback(const int & nbOfChannels,
+	                                   const int & nbrOfSamplesByChannel,
+	                                   const AL_SOUND_FORMAT * buffer,
+	                                   const ALValue & timeStamp)
+	{
+	  /// Compute the maximum value of the front microphone signal.
+	  int maxValueFront = 0;
+	  for(int i = 0 ; i < nbrOfSamplesByChannel ; i++)
+	  {
+	    if(buffer[i] > maxValueFront)
+	    {
+	      maxValueFront = buffer[i];
+	    }
+	  }
+	
+	  /// Do stuff when it gets too noisy
+	  if(maxValueFront > 20)
+	  {
+	    // Interrupt speech engine
+	    // Change mood
+	    // Say something that reflects your mood
+	    // Tell speech engine to continue
+	  }
+	}
 	
 	/// Run a behaviour asynchronously.
 	/**
